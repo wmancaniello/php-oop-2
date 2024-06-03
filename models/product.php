4f6cee6c3019c6af51d3f317db2cae5033c961af
wmancaniello <wmancaniello@gmail.com>
2024-06-03 18:04:05 +0200

    need to change

--- models/product.php
+++ models/product.php
@@ -3,22 +3,3 @@
 <?php
 require_once __DIR__ . "/item.php";
 
-class Product extends Item {
-    private $category;
-    private $itemType;
-
-    public function __construct($title, $price, $image, Category $category, ItemType $itemType) {
-        parent::__construct($title, $price, $image);
-        $this->category = $category;
-        $this->itemType = $itemType;
-    }
-
-    public function getCategory() {
-        return $this->category;
-    }
-
-    public function getItemType() {
-        return $this->itemType;
-    }
-}
-?>
